e3d8d21c7b6e52414ab038f50275799fa2abc0fd
Krivchenko Vyacheslav <krivchenko.vi74@gmail.com>
2026-02-16 21:59:54 +0500

    post creation date added

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,16 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private int likes;
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
         likes = 0;
+        this.creationDate = creationDate;
     }
 
     public String getText() {
@@ -15,5 +19,9 @@ public class Post {
 
     public int getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostsService.java
+++ src/main/java/com/example/demo/service/PostsService.java
@@ -4,14 +4,15 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostsService {
     public List<Post> listAllPosts() {
-        Post post1 = new Post("post 1");
-        Post post2 = new Post("post 2");
-        Post post3 = new Post("post 3");
+        Post post1 = new Post("post 1", new Date());
+        Post post2 = new Post("post 2", new Date(135235524434L));
+        Post post3 = new Post("post 3", new Date(128536563253L));
 
         return new ArrayList<>(List.of(post1, post2, post3));
     }
